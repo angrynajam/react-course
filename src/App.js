38b624042e2e6668f0f87d4b71229966a6563a32
Angry Najam <najamiftikhar2020@gmail.com>
2020-05-30 12:12:37 +0500

    state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Najam</h1>
-      <Person name="Najam" age="19">
-        <h5>My Hobby is programming</h5>
-      </Person>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Najam', age: 19 },
+      { name: 'Hizbi', age: 15 },
+      { name: 'Ash', age: 18 },
+    ],
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'AngryNajam', age: 19 },
+        { name: 'Hizbi', age: 15 },
+        { name: 'Ash', age: 19 },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello Najam</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
